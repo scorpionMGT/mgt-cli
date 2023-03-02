dededd876fe3880632ecc79e60b00d5904dd3b12
毛国涛 <1142656486@qq.com>
2023-03-02 14:17:15 +0800

    feat:  完善基本功能

--- cli.js
+++ cli.js
@@ -5,71 +5,55 @@
 // 如果是Linux 或者 macOS 系统下还需要修改此文件的读写权限为 755
 // 具体就是通过 chmod 755 cli.js 实现修改
 
-// const program = require('commander')
-
-// const run = async () => {
-//   const inquirer = await import('inquirer')
-//   // 定义命令和参数
-//   program.default
-//     .command('create <app-name>')
-//     .description('create a new project')
-//     // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-//     .option('-f, --force', 'overwrite target directory if it exist')
-//     .action((name, options) => {
-//       // 打印执行结果
-//       console.log('name:',name,'options:',options)
-//     })
-  
-//   // 配置版本号信息
-//   program
-//     .version(`v${require('../package.json').version}`)
-//     .usage('<command> [option]')
-  
-//   // 解析用户执行命令传入参数
-//   program.parse(process.argv);
-
-//   inquirer.default.prompt([
-//     {
-//       type: 'input', // type： input, number, confirm, list, checkbox ... 
-//       name: 'name', // key 名
-//       message: 'Your name', // 提示信息
-//       default: 'mgt-cli' // 默认值
-//     }
-//   ]).then(answers => {
-//     // 打印互用输入结果
-//     console.log(answers)
-//   })
-// }
-
-// run()
-
 const program = require('commander')
 const figlet = require('figlet')
 
 program
   // 定义命令和参数
-  .command('create <app-name>')
-  .description('create a new project')
+  .command('template <app-name>')
+  .description('create a new template')
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
   .option('-f, --force', 'overwrite target directory if it exist')
   .action((name, options) => {
-     // 在 create.js 中执行创建任务
-     require('./lib/create.js')(name, options)
+     // 在 template.js 中执行创建任务
+     require('./lib/template.js')(name, options)
   })
-  
+
+// 配置 config 命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+
+// 配置 ui 命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    console.log(option)
+  })
+
 program
    // 配置版本号信息
   .version(`v${require('./package.json').version}`)
   .usage('<command> [option]')
   
-// 解析用户执行命令传入参数
-program.parse(process.argv);
 
-figlet('mgt-cli!!!', function(err, data) {
+figlet.text('mgt-cli', function(err, data) {
     if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
+        console.log('Something went wrong...')
+        console.dir(err)
+        return
     }
-    console.log(data)
-});
+    console.log(data);
+    // 解析用户执行命令传入参数
+    program.parse(process.argv)
+})
+
+
