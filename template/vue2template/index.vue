f8fd6a8cbe096a91302033bf61a16527207fc9b2
毛国涛 <1142656486@qq.com>
2023-03-03 15:06:23 +0800

    feat: 创建vue模版

---
+++ template/vue2template/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div class='pro-table'>
+<el-form class='pro-table-search'>
+<el-form-item label='label'>
+</el-form-item>
+</el-form>
+<div class='pro-table-wrapper'>
+</div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component<componentName>({
+name: 'componentName',
+components: {
+
+},
+})
+export default class componentName extends Vue {
+//生命周期 - 创建完成（访问当前this实例）
+created () {
+
+}
+//生命周期 - 挂载完成（访问DOM元素）
+mounted () {
+
+}
+}
+</script>
+<style lang='scss' scoped>
+@import "@/styles/variables.scss";
+
+</style>
